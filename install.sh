421d4303ffa7640e7a41f265b6bdde8ef94f89d2
Adi M <adim.moldovan@gmail.com>
2017-11-27 11:21:22 +0200

    Created install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+serverpath=/var/www/html
+#mkdir $serverpath
+rsync -av --progress ./ $serverpath --exclude '.git' --exclude '.gitignore' --exclude 'install.sh'
+cd $serverpath
+npm install
+rm $serverpath/package.json
