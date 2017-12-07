ed07adce932d01598d8432fd88887fbed1505465
Adi M <adim.moldovan@gmail.com>
2017-12-07 14:38:57 +0200

    Updated install script. Fixed EOL

--- install.sh
+++ install.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-serverpath=
+serverpath=/var/lib/tomcat8/webapps
 basepath=$(dirname $(readlink -f $0))
 basedir=$(basename $basepath)
-
-echo -e "\nPlease select one option:\n"
-echo -e "\t 1 - Install on Apache"
-echo -e "\t 2 - Install on Tomcat 8"
-read opt
-
-if [ $opt = 1 ]
-    then
-        serverpath=/var/www/html
-elif [ $opt = 2 ]
-    then 
-        serverpath=/var/lib/tomcat8/webapps
-else
-    echo "Invalid option selected"
-    exit
-fi
-
 finalpath=$serverpath/$basedir
 
+echo -e "\nUsing path $finalpath"
 echo "Removing $finalpath"
 rm -rf $finalpath
 mkdir $finalpath
@@ -29,7 +13,7 @@ mkdir $finalpath
 echo "Copying files to $finalpath"
 rsync -av --progress $basepath $serverpath --exclude '.git' --exclude '.gitignore' --exclude 'install.sh'
 cd $finalpath
-echo "Intalling"
+echo "Installing"
 npm install
-echo "Cleaning up"
+echo "Cleaning up garbage in $finalpath"
 rm package*.json
